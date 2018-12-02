208805ad904c382217ac56b0d2fe2be8a4b8356a
sunginHwang <gommpo111@gmail.com>
2018-12-02 19:00:17 +0900

    redux 연동 성공!!!

--- container/CounterContainer.tsx
+++ container/CounterContainer.tsx
@@ -2,43 +2,43 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import {StoreState} from '../core/store/modules';
-
-import {actionCreators} from '../core/store/actions/CounterAction';
+import { actionCreators as counterActions } from '../core/store/actions/CounterAction';
 
 
 class CountContainer extends Component<{
     value: number;
-    CounterAction: typeof actionCreators;
+    CounterActions: typeof counterActions;
 }> {
     constructor(props) {
         super(props);
     }
 
     componentDidUpdate(){
-        console.log(this.props);
     }
 
     onCountIncreaseClick = () =>{
         const {props} = this as any;
-        const { CounterAction } = props;
-        CounterAction.increment();
+        const { CounterActions } = props;
+        CounterActions.increment();
     };
 
     render() {
-        console.log(this.props);
+        const {props} = this as any;
+
         return(
             <div>
                 <button onClick={()=>this.onCountIncreaseClick()}>버튼클릭</button>
-                <p>{this.props.value}</p>
+                <p>{props.value}</p>
             </div>
         );
     }
 }
+
 export default connect(
     ({ counter }: StoreState) => ({
         value: counter.value
     }),
     (dispatch) => ({
-        CounterAction: bindActionCreators(actionCreators, dispatch)
+        CounterActions: bindActionCreators(counterActions, dispatch)
     })
 )(CountContainer as any);
--- core/store/modules/index.ts
+++ core/store/modules/index.ts
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
-import CounterReducer from '../modules/CounterReducer';
-import { ICounterInfo }  from '../../../model/interface/ICounterInfo';
+import counter from './CounterReducer';
+import {ICounterInfo} from '../../../model/interface/ICounterInfo';
+
 
 export default combineReducers({
-    CounterReducer
+    counter
 } as any);
 
 // 스토어의 상태 타입 정의
